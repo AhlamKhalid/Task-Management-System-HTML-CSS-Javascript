cf1ee43e647e86234637d6e86e447aee6df158ea
AhlamKhalid <ahlamkarkashan@gmail.com>
2023-03-04 03:33:20 +0300

    add simple actions using js

---
+++ js/main.js
@@ -0,0 +1,67 @@
+// elements
+const radioViewOptions = document.querySelectorAll("input[name='view-option']");
+const listView = document.getElementById("list-view");
+const boardView = document.getElementById("board-view");
+const addTaskCTA = document.getElementById("add-task-cta");
+const setTaskOverlay = document.getElementById("set-task-overlay");
+const closeButtons = document.querySelectorAll(".close-button");
+const statusSelect = document.getElementById("status-select");
+const statusDropdown = document.getElementById("status-dropdown");
+const taskItems = document.querySelectorAll(".task-item");
+const viewTaskOverlay = document.getElementById("view-task-overlay");
+// the current active overlay
+let activeOverlay = null;
+
+//** event listeners **//
+
+// radio buttons for view option
+radioViewOptions.forEach((radioButton) => {
+  radioButton.addEventListener("change", (event) => {
+    const eventTarget = event.target;
+    const viewOption = eventTarget.value;
+
+    switch (viewOption) {
+      case "list":
+        boardView.classList.add("hide");
+        listView.classList.remove("hide");
+        break;
+      case "board":
+        listView.classList.add("hide");
+        boardView.classList.remove("hide");
+        break;
+    }
+  });
+});
+
+// add task
+addTaskCTA.addEventListener("click", () => {
+  setTaskOverlay.classList.remove("hide");
+  activeOverlay = setTaskOverlay;
+  // disable scrolling for content behind the overlay
+  document.body.classList.add("overflow-hidden");
+});
+
+// close buttons inside overlays
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    activeOverlay.classList.add("hide");
+    activeOverlay = null;
+    // reenable scrolling
+    document.body.classList.remove("overflow-hidden");
+  });
+});
+
+// open status dropdown
+statusSelect.addEventListener("click", () => {
+  statusDropdown.classList.toggle("hide");
+});
+
+// click a task
+taskItems.forEach((task) => {
+  task.addEventListener("click", () => {
+    viewTaskOverlay.classList.remove("hide");
+    activeOverlay = viewTaskOverlay;
+    // disable scrolling for content behind the overlay
+    document.body.classList.add("overflow-hidden");
+  });
+});
